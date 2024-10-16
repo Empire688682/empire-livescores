169ccd98cf7c64aaaf507fb992ecfd2f9fb722a0
Empire688682 <asehindej@gmail.com>
2024-10-16 23:56:44 +0100

    data

--- empire-livescore/src/Component/data.js
+++ empire-livescore/src/Component/data.js
@@ -23,6 +23,12 @@ export const subNavbarData = [
 
 const sideBarData = [
     {
-        region:[{title:"England", img:""}]
+        region:[
+            {title:"England", imgSrc:"flag-icon flag-icon-eng"},
+            {title:"Spain", imgSrc:"flag-icon flag-icon-spa"},
+            {title:"Championship", imgSrc:"flag-icon flag-icon-ngn"},
+            {title:"Germany", imgSrc:"flag-icon flag-icon-ger"},
+            {title:"Italy", imgSrc:"flag-icon flag-icon-ita"},
+        ]
     }
 ]
