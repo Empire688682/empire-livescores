7c85eb19729ec49a5035cb8c82d30b8fbf606d2d
Empire688682 <asehindej@gmail.com>
2024-10-21 11:31:28 +0100

    single news

---
+++ empire-livescore/src/app/news/[newsId]/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './newsId.module.css';
+
+const page = () => {
+  return (
+    <div className={style.single_news}>
+      <h1>
+        Single News Page
+      </h1>
+    </div>
+  )
+}
+
+export default page
