4de9b6ce991d6cb601f9855d2bfef5e5f3a4c0ef
Empire688682 <asehindej@gmail.com>
2024-10-21 09:44:03 +0100

    about

--- empire-livescore/src/app/page.js
+++ empire-livescore/src/app/page.js
@@ -2,12 +2,14 @@ import FootballCom from '@/Component/FootballCom/FootballCom';
 import MainDate from '@/Component/MainDate/MainDate';
 import React from 'react';
 import style from './page.module.css';
+import MatchAfter from '@/Component/MatchAfter/MatchAfter';
 
 const page = () => {
   return (
     <div className={style.page}>
       <MainDate/>
       <FootballCom/>
+      <MatchAfter/>
     </div>
   )
 }
