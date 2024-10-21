a48850060f2876bfd1e56e625a9072230cd184e9
Empire688682 <asehindej@gmail.com>
2024-10-21 11:24:36 +0100

    news

---
+++ empire-livescore/src/app/news/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './news.module.css';
+import NewsCom from '@/Component/NewsCom/NewsCom';
+
+const page = () => {
+  return (
+    <div className={style.news}>
+      <NewsCom/>
+    </div>
+  )
+}
+
+export default page
