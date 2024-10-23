dcaf9b023c6b202f16a97eb6545fc93e342b9c6e
Empire688682 <asehindej@gmail.com>
2024-10-23 23:47:48 +0100

    submenu

--- empire-livescore/src/Component/SubNavbar/SubNavbar.jsx
+++ empire-livescore/src/Component/SubNavbar/SubNavbar.jsx
@@ -1,16 +1,20 @@
+'use client'
 import React from 'react';
 import style from './SubNavbar.module.css';
 import { subNavbarData } from '../data';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const SubNavbar = () => {
+    const pathName = usePathname();
+
     return (
         <div className={style.subNavbar}>
             <ul>
                 {
                     subNavbarData.map((item) => (
                         <li key={item.title}>
-                            <Link className={style.links} href={item.path}>{item.title}</Link>
+                            <Link className={pathName === item.path ? `${style.link} ${style.active}` : style.link} href={item.path}>{item.title}</Link>
                         </li>
                     ))
                 }
