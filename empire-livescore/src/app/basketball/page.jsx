e75166e412f54e12a8b15889f7809061f08712a6
Empire688682 <asehindej@gmail.com>
2024-10-23 15:25:32 +0100

    bug free

--- empire-livescore/src/app/basketball/page.jsx
+++ empire-livescore/src/app/basketball/page.jsx
@@ -8,45 +8,54 @@ import axios from 'axios';
 
 const page = () => {
   const [data, setData] = useState([]);
+  const [loading, setLoding] = useState(false)
 
   const fetchData = async () => {
+    setLoding(true);
     try {
       const response = await axios.get("https://v1.basketball.api-sports.io/games", {
         headers: {
           "x-rapidapi-host": "v3.football.api-sports.io",
           "x-rapidapi-key": process.env.NEXT_PUBLIC_API
         },
-        params:{
+        params: {
           date: new Date().toISOString().slice(0, 10)
         }
-        
+
       });
-      console.log(response);
       if (response) {
         setData(response.data.response);
       }
     } catch (error) {
       console.log("Error:", error);
     }
+    finally {
+      setLoding(false);
+    }
   };
 
-  console.log("Data:", data);
-  useEffect(()=>{
+  useEffect(() => {
     fetchData();
     console.log(new Date().toISOString().slice(0, 10));
-  },[])
+  }, [])
   return (
-    <div className={style.page}>
-      <MainDate/>
+    <div className={style.basketball}>
+      <MainDate />
       {
-        data.map((data)=>{
-          return (
-            <div>
-              <LeagueCom country={data.country.name} league={data.league.name} leagueLogo={data.league.logo}/>
-              <MatchAfter team1Logo={data.teams.home.logo} team2Logo={data.teams.away.logo} team1={data.teams.home.name} team2={data.teams.away.name} time={data.time}/>
-            </div>
-          )
-        })
+        loading ? <h2>LOADING.....</h2>
+          :
+          <>
+            {
+              data.map((data) => {
+                return (
+                  <div>
+                    <LeagueCom country={data.country.name} league={data.league.name} leagueLogo={data.league.logo} />
+                    <MatchAfter team1Logo={data.teams.home.logo} team2Logo={data.teams.away.logo} team1={data.teams.home.name} team2={data.teams.away.name} time={data.time} />
+                  </div>
+                )
+              })
+            }
+          </>
       }
     </div>
   )
