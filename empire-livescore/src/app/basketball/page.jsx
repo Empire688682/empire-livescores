3261c3b1ad3c89305b250c8e1361cb39acd759ba
Empire688682 <asehindej@gmail.com>
2024-10-24 01:03:32 +0100

    error UI

--- empire-livescore/src/app/basketball/page.jsx
+++ empire-livescore/src/app/basketball/page.jsx
@@ -8,7 +8,9 @@ import axios from 'axios';
 
 const page = () => {
   const [data, setData] = useState([]);
-  const [loading, setLoding] = useState(false)
+  const [loading, setLoding] = useState(false);
+  const [limitExceeded, setLimitExceeded] = useState(false);
+  const [networkError, setNetworkError] = useState('');
 
   const fetchData = async () => {
     setLoding(true);
@@ -23,16 +25,23 @@ const page = () => {
         }
 
       });
+      if (response.data.errors) {
+        setLimitExceeded(true);
+      }
+      console.log(response);
       if (response) {
         setData(response.data.response);
       }
     } catch (error) {
+      setNetworkError(error.message);
       console.log("Error:", error);
     }
     finally {
       setLoding(false);
     }
   };
+
+  console.log(data)
 
   useEffect(() => {
     fetchData();
@@ -45,14 +54,34 @@ const page = () => {
           :
           <>
             {
-              data.map((data) => {
-                return (
-                  <div>
-                    <LeagueCom country={data.country.name} league={data.league.name} leagueLogo={data.league.logo} />
-                    <MatchAfter team1Logo={data.teams.home.logo} team2Logo={data.teams.away.logo} team1={data.teams.home.name} team2={data.teams.away.name} time={data.time} />
-                  </div>
-                )
-              })
+              networkError ? <div className={style.limitExceeded}>
+                <h2>{networkError} 🚫</h2>
+                <p>Please check your connection and try again.</p>
+                </div>
+                :
+                <>
+                  {
+                    limitExceeded ? <div className={style.limitExceeded}>
+                      <h2>Daily Request Limit Reached 🚫</h2>
+                      <p>Thank you for using E-Live Score app! Unfortunately, we've exceeded the daily limit of 100 requests provided by the free API we use to fetch live match data.</p>
+                      <p>Since the app is built using a free service, we are only allowed to make 100 requests per day. We apologize for the inconvenience and invite you to check back tomorrow when the request limit resets.</p>
+                      <p>We appreciate your support and understanding!</p>
+                    </div>
+                      :
+                      <>
+                        {
+                          data.map((data) => {
+                            return (
+                              <div>
+                                <LeagueCom country={data.country.name} league={data.league.name} leagueLogo={data.league.logo} />
+                                <MatchAfter team1Logo={data.teams.home.logo} team2Logo={data.teams.away.logo} team1={data.teams.home.name} team2={data.teams.away.name} time={data.time} />
+                              </div>
+                            )
+                          })
+                        }
+                      </>
+                  }
+                </>
             }
           </>
       }
