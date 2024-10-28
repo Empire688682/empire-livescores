b580a6aea5aba6e97e4b623baa6757b54f0ea3d0
Empire688682 <asehindej@gmail.com>
2024-10-28 23:57:55 +0100

    slug

--- empire-livescore/src/app/football/[singleM]/page.jsx
+++ empire-livescore/src/app/football/[singleM]/page.jsx
@@ -8,35 +8,48 @@ import aston_Villa from '../../../public/aston-villa.png';
 
 const page = () => {
     const [match, setMatch] = useState(null);
-    
-    useEffect(()=>{
-        if(typeof window !== "undefined"){
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
             setMatch(JSON.parse(localStorage.getItem("football")))
         }
-    },[])
+    }, [])
     return (
         <div className={style.matchContainer}>
             <div className={style.header}>
                 <div>
-                <Image src={arsenal} alt='logo' width={50} height={50} className={style.teamLogo} />
-                <p>homeTeam</p>
+                    <Image src={arsenal} alt='logo' width={50} height={50} className={style.teamLogo} />
+                    <p>homeTeam</p>
                 </div>
                 <div className={style.scores}>
-                <p>Score:</p>
-                <p>1 - 2</p>
+                    <p>Score:</p>
+                    <p>1 - 2</p>
                 </div>
                 <div>
-                <Image src={aston_Villa} alt='logo' width={50} height={50} className={style.teamLogo} />
-                <p>awayTeam</p>
+                    <Image src={aston_Villa} alt='logo' width={50} height={50} className={style.teamLogo} />
+                    <p>awayTeam</p>
                 </div>
             </div>
 
             <section className={style.section}>
                 <h2>Match Statistics</h2>
                 <ul className={style.statsList}>
-                        <li>
-                            <strong>shoot:</strong> <p>stat.home</p> - <p>stat.away</p>
-                        </li>
+                    <div className={style.half_time}>
+                        <h4>HT</h4>
+                        <div>
+                            <p>1</p>
+                            <p>-</p>
+                            <p>0</p>
+                        </div>
+                    </div>
+                    <div className={style.full_time}>
+                        <h4>FT</h4>
+                        <div>
+                            <p>1</p>
+                            <p>-</p>
+                            <p>2</p>
+                        </div>
+                    </div>
                 </ul>
             </section>
 
@@ -53,14 +66,14 @@ const page = () => {
             <section className={style.section}>
                 <h2>Coaches</h2>
                 <div className={style.coaches}>
-                <div>
-                    <p><strong>Coach:</strong> Daniel</p>
-                    <Image src={arsenal} alt='coach' width={50} height={50} />
-                </div>
-                <div>
-                    <p><strong>Coach:</strong> Juwon</p>
-                    <Image src={aston_Villa} alt='coach' width={50} height={50} />
-                </div>
+                    <div>
+                        <p><strong>Coach:</strong> Daniel</p>
+                        <Image src={arsenal} alt='coach' width={50} height={50} />
+                    </div>
+                    <div>
+                        <p><strong>Coach:</strong> Juwon</p>
+                        <Image src={aston_Villa} alt='coach' width={50} height={50} />
+                    </div>
                 </div>
             </section>
         </div>
