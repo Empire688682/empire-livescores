0575843550a4ef2c7ce089e3afe67824b580f8d5
Empire688682 <asehindej@gmail.com>
2024-10-29 02:46:04 +0100

    data

--- empire-livescore/src/app/football/[match]/page.jsx
+++ empire-livescore/src/app/football/[match]/page.jsx
@@ -28,101 +28,108 @@ const page = () => {
         }
     }, []);
 
-    const fetchMatch = () =>{
-        if(data){
-          const foundMatch = data.find((match) => match.fixture.id === parseInt(id));
-          if(foundMatch){
-            setMatch(foundMatch);
-            console.log(foundMatch);
-          }
-          else{
-            console.log("No match found")
-          }
+    const fetchMatch = () => {
+        if (data) {
+            const foundMatch = data.find((match) => match.fixture.id === parseInt(id));
+            if (foundMatch) {
+                setMatch(foundMatch);
+                console.log(foundMatch);
+            }
+            else {
+                console.log("No match found")
+            }
         }
-        else{
-           console.log("No match found with the given ID");
+        else {
+            console.log("No match found with the given ID");
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchMatch();
-    },[data]);
+    }, [data]);
 
     console.log(match.fixture)
 
 
     return (
         <div className={style.matchContainer}>
-            <div className={style.header}>
-                <div>
-                    <Image src={arsenal} alt='logo' width={50} height={50} className={style.teamLogo} />
-                    <p>match.teams.home.name</p>
-                </div>
-                <div className={style.scores}>
-                    <p>Score:</p>
-                    <p>1 - 2</p>
-                </div>
-                <div>
-                    <Image src={aston_Villa} alt='logo' width={50} height={50} className={style.teamLogo} />
-                    <p>match.teams.home.name</p>
-                </div>
-            </div>
-
-            <section className={noticeClear? `${style.notice_section} ${style.clear}`:style.notice_section}>
-                <LiaTimesSolid className={style.cross_icon} onClick={()=>setNoticeClear(true)}/>
-                <ScoreNotice/>
-            </section>
-
-            <section className={style.section}>
-                <h2>Match Statistics</h2>
-                <ul className={style.statsList}>
-                    <div className={style.half_time}>
-                        <h4>HT</h4>
-                        <div>
-                            <p>1</p>
-                            <p>-</p>
-                            <p>0</p>
+            {
+                match && match.fixture?
+                    <>
+                        <div className={style.header}>
+                            <div>
+                                <Image src={match.teams.home.logo} alt='logo' width={50} height={50} className={style.teamLogo} />
+                                <p>{match.teams.home.name}</p>
+                            </div>
+                            <div className={style.scores}>
+                                <p>Score:</p>
+                                <p>1 - 2</p>
+                            </div>
+                            <div>
+                                <Image src={match.teams.away.logo} alt='logo' width={50} height={50} className={style.teamLogo} />
+                                <p>{match.teams.away.name}</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className={style.full_time}>
-                        <h4>FT</h4>
-                        <div>
-                            <p>1</p>
-                            <p>-</p>
-                            <p>2</p>
-                        </div>
-                    </div>
-                </ul>
-            </section>
 
-            <section className={style.section_stadium}>
-                <h2>Stadium Information</h2>
-                <p><strong>Name:</strong> stadium.name</p>
-                <p><strong>Location:</strong> stadium.location</p>
-                <p><strong>Capacity:</strong> stadium.capacity</p>
-                <div className={style.imageWrapper}>
-                    <Image src={arsenal} alt='stadium' width={400} height={250} />
-                </div>
-            </section>
+                        <section className={noticeClear ? `${style.notice_section} ${style.clear}` : style.notice_section}>
+                            <LiaTimesSolid className={style.cross_icon} onClick={() => setNoticeClear(true)} />
+                            <ScoreNotice />
+                        </section>
 
-            <section className={style.section_stadium}>
-                <h2>Refree Name</h2>
-                <p><strong>Refree:</strong> Olive</p>
-            </section>
+                        <section className={style.section}>
+                            <h2>Match Statistics</h2>
+                            <ul className={style.statsList}>
+                                <div className={style.half_time}>
+                                    <h4>HT</h4>
+                                    <div>
+                                        <p>1</p>
+                                        <p>-</p>
+                                        <p>0</p>
+                                    </div>
+                                </div>
+                                <div className={style.full_time}>
+                                    <h4>FT</h4>
+                                    <div>
+                                        <p>1</p>
+                                        <p>-</p>
+                                        <p>2</p>
+                                    </div>
+                                </div>
+                            </ul>
+                        </section>
 
-            <section className={style.section}>
-                <h2>Coaches</h2>
-                <div className={style.coaches}>
-                    <div>
-                    <Image src={arsenal} alt='coach' width={50} height={50} />
-                        <p><strong>Coach:</strong> Daniel</p>
-                    </div>
-                    <div>
-                    <Image src={aston_Villa} alt='coach' width={50} height={50} />
-                        <p><strong>Coach:</strong> Juwon</p>
-                    </div>
-                </div>
-            </section>
+                        <section className={style.section_stadium}>
+                            <h2>Stadium Information</h2>
+                            <p><strong>Name:</strong> stadium.name</p>
+                            <p><strong>Location:</strong> stadium.location</p>
+                            <p><strong>Capacity:</strong> stadium.capacity</p>
+                            <div className={style.imageWrapper}>
+                                <Image src={arsenal} alt='stadium' width={400} height={250} />
+                            </div>
+                        </section>
+
+                        <section className={style.section_stadium}>
+                            <h2>Refree Name</h2>
+                            <p><strong>Refree:</strong> Olive</p>
+                        </section>
+
+                        <section className={style.section}>
+                            <h2>Coaches</h2>
+                            <div className={style.coaches}>
+                                <div>
+                                    <Image src={arsenal} alt='coach' width={50} height={50} />
+                                    <p><strong>Coach:</strong> Daniel</p>
+                                </div>
+                                <div>
+                                    <Image src={aston_Villa} alt='coach' width={50} height={50} />
+                                    <p><strong>Coach:</strong> Juwon</p>
+                                </div>
+                            </div>
+                        </section>
+                    </>
+                    :
+                    <h3>Loading......</h3>
+            }
         </div>
     );
 }
